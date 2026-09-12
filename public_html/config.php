bab2709ba8a1a006a767cce4270f1b389c35696d
Ranjeet <ranjitkumar123bind@gmail.com>
2026-09-12 17:27:40 +0530

    Clear database configuration values
    Updated database credentials to empty values.

--- public_html/config.php
+++ public_html/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u174340608_root');
-define('DB_PASSWORD', 'Ranjeet8810@123');
-define('DB_NAME', 'u174340608_carpet');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  // correct & fixed port
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
